e067ebe388c533b3b041bee0b1beecc488eef351
Julio <julio@icabian.org>
2020-03-17 00:02:48 +0100

    Modified tile sources to be external ones

--- dashboard/main.py
+++ dashboard/main.py
@@ -234,7 +234,7 @@ def create_map(indices_meses):
                 show_legend=False,
                 tools=[hover_points])
 
-    tiles = gv.tile_sources.CartoLight
+    tiles = gv.tile_sources.WMTS("https://b.basemaps.cartocdn.com/light_nolabels/{Z}/{X}/{Y}@2x.png", name="Carto_light_Nolabels")
 
     map = tiles * points
 
